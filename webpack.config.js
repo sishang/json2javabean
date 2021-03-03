934191eeab5a6bbba2f77c4be0f44748c7cc0822
zss <zhangsishang3@jd.com>
2021-03-03 14:26:42 +0800

    fix: 修改输出文件

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'json2javabean.js',
     library: 'package',
     libraryTarget: 'umd'
   },
