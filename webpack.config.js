f8d283b4eb6ce94f73b89dc6643d0b8e64bcd1d6
zss <zhangsishang3@jd.com>
2021-03-04 19:26:57 +0800

    fix: bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'none',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'lib'),
     filename: "main.js",
     library: 'json2java',
-    libraryTarget: 'umd',
-    globalObject: 'this'
+    libraryTarget: "umd",
+    libraryExport: "default",
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : typeof windows !== 'undefined' ? window : typeof global !== 'undefined' ? global : this"
   },
   module: {
     rules: [{
